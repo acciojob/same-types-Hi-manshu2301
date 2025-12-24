c13af37b84e8ad812468f09db9764b3048d5893a
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2025-12-24 16:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function isSameType(value1, value2) {
 	if(Number.isNaN(value1) || Number.isNaN(value2)){
 		return false;
 	}
+	if(Number.isNaN(value1) && Number.isNaN(value2)){
+		return true;
+	}
 	return typeof value1 === typeof value2
 }
 
